5d44fc0127496ccee215817d5b379f4e0aaa6351
Daniel Burnette <dcburnette999@gmail.com>
2014-10-20 10:28:21 -0500

    Setup devise to allow login via username instead of just email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,25 @@
 class User < ActiveRecord::Base
-  
+
   attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  #For adding the ability to log in with username instead of just email
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  validates :username,
+  :uniqueness => {
+    :case_sensitive => false
+  },
+  :format => { ... } # etc.
 
 end
