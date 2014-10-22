08606bc6f859517908b256188375a68253051ea3
Daniel Burnette <dcburnette999@gmail.com>
2014-10-22 12:31:16 -0500

    16p clicks are working. Time to work on the AJAX requests to update the matches!

--- app/assets/javascripts/playTournament.js
+++ app/assets/javascripts/playTournament.js
@@ -16,10 +16,10 @@ $('.player').on('click', function() {
 	$(otherPlayer).css('background-color', 'red');
 
 //////////////////
+	var winnerPosition = $allPlayerSpots.length-1;
+	var winner = $allPlayerSpots.get(winnerPosition);
 	if (tournamentSize == 4){
 		var $roundTwoSpots = $allPlayerSpots.slice(4,6);
-		var winnerPosition = $allPlayerSpots.length-1;
-		var winner = $allPlayerSpots.get(winnerPosition);
 		if (playerPosition == 0 || playerPosition == 1){
 			var spotToFill = $roundTwoSpots.get(0);
 		}
@@ -31,15 +31,40 @@ $('.player').on('click', function() {
 		}
 	}
 	else if (tournamentSize == 8){
-
+		var $roundTwoSpots = $allPlayerSpots.slice(8,12);
+		var $roundThreeSpots = $allPlayerSpots.slice(12,14);
+		if (playerPosition == 0 || playerPosition == 1) {
+			var spotToFill = $roundTwoSpots.get(0);
+		}
+		else if (playerPosition == 2 || playerPosition == 3) {
+			var spotToFill = $roundTwoSpots.get(1);
+		}
+		else if (playerPosition == 4 || playerPosition == 5) {
+			var spotToFill = $roundTwoSpots.get(2);
+		}
+		else if (playerPosition == 6 || playerPosition == 7) {
+			var spotToFill = $roundTwoSpots.get(3);
+		}
+		else if (playerPosition == 8 || playerPosition == 9) {
+			var spotToFill = $roundThreeSpots.get(0);
+		}
+		else if (playerPosition == 10 || playerPosition == 11) {
+			var spotToFill = $roundThreeSpots.get(1);
+		}
+		else if (playerPosition == 12 || playerPosition == 13) {
+			var spotToFill = winner;
+		}
 	}
 	else if (tournamentSize == 16){
-		//fill in later
+		//fill in later once I make that view
+		//////////////////////////////////////////////
+		//////////////////////////////////////////////
+		//////////////////////////////////////////////
 	}
 
-/////////////////
 	var playerName = $(this).text();
 	$(spotToFill).text(playerName);
+
 	//Special case, illuminate champion spot upon choosing victor of finals 
 	if (playerPosition+1 == winnerPosition || playerPosition+2 == winnerPosition) {
 		$(winner).css('background-color', 'green');
