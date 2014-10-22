2c9148d88993b458d9bfc7d4899265e1f3b345bd
Daniel Burnette <dcburnette999@gmail.com>
2014-10-22 12:19:52 -0500

    four clicks working

--- app/assets/javascripts/playTournament.js
+++ app/assets/javascripts/playTournament.js
@@ -1,23 +1,49 @@
 
 //Making buttons illuminate upon clicks 
 $('.player').on('click', function() {
-	$allPlayerSpots = $('.player');
-	playerPostition = $allPlayerSpots.index($(this))
+	var $allPlayerSpots = $('.player');
+	var tournamentSize = ($allPlayerSpots.size()+1)/2;
+	var playerPosition = $allPlayerSpots.index($(this))
 
 	if ($(this).hasClass('odd')){
-		var otherPlayer = $allPlayerSpots.get(playerPostition-1)
+		var otherPlayer = $allPlayerSpots.get(playerPosition-1)
 	}
+
 	else if ($(this).hasClass('even')){
-		var otherPlayer = $allPlayerSpots.get(playerPostition+1)
+		var otherPlayer = $allPlayerSpots.get(playerPosition+1)
 	}
 	$(this).css('background-color', 'green')
 	$(otherPlayer).css('background-color', 'red');
 
-	//Special case, illuminate champion spot upon chooing victor of finals 
-	var winnerPosition = $allPlayerSpots.length-1;
-	if (playerPostition+1 == winnerPosition || playerPostition+2 == winnerPosition) {
+//////////////////
+	if (tournamentSize == 4){
+		var $roundTwoSpots = $allPlayerSpots.slice(4,6);
+		var winnerPosition = $allPlayerSpots.length-1;
 		var winner = $allPlayerSpots.get(winnerPosition);
+		if (playerPosition == 0 || playerPosition == 1){
+			var spotToFill = $roundTwoSpots.get(0);
+		}
+		else if (playerPosition == 2 || playerPosition == 3){
+			var spotToFill = $roundTwoSpots.get(1);
+		}
+		else if(playerPosition == 4 || playerPosition == 5){
+			var spotToFill = winner;
+		}
+	}
+	else if (tournamentSize == 8){
+
+	}
+	else if (tournamentSize == 16){
+		//fill in later
+	}
+
+/////////////////
+	var playerName = $(this).text();
+	$(spotToFill).text(playerName);
+	//Special case, illuminate champion spot upon choosing victor of finals 
+	if (playerPosition+1 == winnerPosition || playerPosition+2 == winnerPosition) {
 		$(winner).css('background-color', 'green');
+
 	}
 
 });
