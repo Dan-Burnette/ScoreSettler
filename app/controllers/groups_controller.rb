2a945d3fd51fde3d9bad988b653b679029b25993
Daniel Burnette <dcburnette999@gmail.com>
2014-10-24 10:14:55 -0500

    Fixed all the stats errors. I think

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -36,29 +36,39 @@ class GroupsController < ApplicationController
     @all_user_losses = []
     @all_user_win_loss_ratios = []
     @all_user_tournament_wins = []
-    @group_tournaments = @group.tournaments
-    @group_tournaments.each do |t|
-      @group.users.each do |user|
+
+    @group.users.each do |user|
+       @all_user_wins.push(0)
+       @all_user_losses.push(0)
+       user_tournament_wins = Tournament.where("champion_id = ? AND group_id = ?", user.id, @group.id).count
+       @all_user_tournament_wins.push(user_tournament_wins)
+    end
+
+    @group.tournaments.each_with_index do |t, i|
+      @group.users.each_with_index do |user, j|
         #Check for nils because we don't want to count bye matches as wins for that player
         user_match_wins = Match.where("winner_id = ? AND tournament_id = ?", user.id, t.id).where.not(player_1: nil).where.not(player_2: nil).count
-        @all_user_wins.push(user_match_wins)
+        @all_user_wins[j] += user_match_wins
         user_match_losses = Match.where("player_1 = ? OR player_2 = ? AND tournament_id = ?" , user.id, user.id, t.id).where.not(winner_id: user.id).count
-        @all_user_losses.push(user_match_losses)
-        user_tournament_wins = Tournament.where("champion_id = ? AND group_id = ?", user.id, @group.id).count
-        @all_user_tournament_wins.push(user_tournament_wins)
-        #Calculate win/loss ratio. Need to avoid divide by zero error
-        if (user_match_losses == 0)
-          user_win_loss_ratio = "Unbeatable"
-          if (user_match_wins == 0)
-            user_win_loss_ratio = 0.0
-          end
-        else
-          user_win_loss_ratio = ((user_match_wins.to_f/user_match_losses.to_f)*100).to_i / 100.0
-        end
-        @all_user_win_loss_ratios.push(user_win_loss_ratio)
+        @all_user_losses[j] += user_match_losses       
       end
-
     end
+
+    #Calculate win/loss ratio. Need to avoid divide by zero error
+    @group.users.each_with_index do |user, i|
+      user_match_wins = @all_user_wins[i]
+      user_match_losses = @all_user_losses[i]
+      if (user_match_losses == 0)
+        user_win_loss_ratio = "Unbeatable"
+        if (user_match_wins == 0)
+          user_win_loss_ratio = 0.0
+        end
+      else
+        user_win_loss_ratio = ((user_match_wins.to_f/user_match_losses.to_f)*100).to_i / 100.0
+      end
+      @all_user_win_loss_ratios.push(user_win_loss_ratio)
+    end
+
 
 
 
