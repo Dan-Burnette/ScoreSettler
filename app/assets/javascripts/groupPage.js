683f187caa0ebee35c29348c3d7c144379f9796e
Daniel Burnette <dcburnette999@gmail.com>
2014-10-26 19:02:39 -0500

    Made the amount of players selected display

--- app/assets/javascripts/groupPage.js
+++ app/assets/javascripts/groupPage.js
@@ -5,6 +5,7 @@ $(document).ready(function()
 ); 
 
 
+//Send AJAX to invite a user on click
 $('.invite-user').on('click', function() {
 	var username = $('.user-to-invite').val();
 	var group_id = $('.group-id').val();
@@ -25,6 +26,19 @@ $('.invite-user').on('click', function() {
 		$(me).text("Invite");
 	}, 1000);
 
+})
+
+
+//Keeping track of how many player checkboxes are selected
+var playersSelected = 0;
+$('.check').on('click', function() {
+	if ($(this).is(':checked')) {
+		playersSelected++;
+	 }
+	 else {
+	 	playersSelected--;
+	 }
+	 $('.player-counter').text("Players selected: " + playersSelected);
 })
 
 // Hover for the table to indicate you can click to sort
