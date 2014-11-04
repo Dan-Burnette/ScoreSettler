577ead50e36e91fc183c217e7408804e1f9d5881
Daniel Burnette <dcburnette999@gmail.com>
2014-11-04 15:04:37 -0600

    styled the head to head area

--- app/assets/javascripts/headToHead.js
+++ app/assets/javascripts/headToHead.js
@@ -1,11 +1,10 @@
-
+// When selector changes, get server data for updating the head to head table via AJAX request
 function changeEventHandler(event) {
 
 	var groupId = $('.group-id').val();
 	var user1 = $('.user-1').val();
 	var user2 = $('.user-2').val();
 	var URL = groupId +',' + user1 + ',' + user2;
-
 
 	$.ajax({
 		url: '/head_to_head/' + URL
