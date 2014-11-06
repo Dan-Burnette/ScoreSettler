6a14b2285f5ff20bfbc14e4770d49e481aba86ac
Daniel Burnette <dcburnette999@gmail.com>
2014-11-06 10:32:18 -0600

    logic for 4p double elimination is working as desired

--- app/assets/javascripts/playDoubleElimTournament.js
+++ app/assets/javascripts/playDoubleElimTournament.js
@@ -1,4 +1,4 @@
-//FOR DOUBLE ELIMINATION TOURNAMENTS
+// FOR DOUBLE ELIMINATION TOURNAMENTS
 //Making buttons illuminate upon clicks appropriately and send AJAX to 
 //server to update the state of the tournament
 //"de" short for double eliminaton in classes in the view DE views
@@ -19,7 +19,7 @@ $('.player-de').on('click', function() {
 
   else if ($(this).hasClass('even')){
     var otherPlayer = $allPlayerSpots.get(playerPosition+1);
-    var whichPlayer = 'player1'
+    var whichPlayer = 'player1';
   }
 
   $(this).css('background-color', 'green');
@@ -35,12 +35,14 @@ $('.player-de').on('click', function() {
     var $roundThreeSpots = $allPlayerSpots.slice(10,12);
     if (playerPosition == 0 || playerPosition == 1){
       var spotToFill = $roundTwoSpots.get(0);
+      var loserSpotToFill = $allPlayerSpots.get(4);
       var match_id = $('.match_1_id').val();
       var next_match = $('.match_4_id').val();
       var next_match_player = 'player1';
     }
     else if (playerPosition == 2 || playerPosition == 3){
       var spotToFill = $roundTwoSpots.get(1);
+      var loserSpotToFill = $allPlayerSpots.get(5);
       var match_id = $('.match_2_id').val();
       var next_match = $('.match_4_id').val();
       var next_match_player = 'player2';
@@ -48,35 +50,42 @@ $('.player-de').on('click', function() {
     else if(playerPosition == 4 || playerPosition == 5){
       var spotToFill = $roundTwoSpots.get(3);
       var match_id = $('.match_3_id').val();
-      var next_match = $('.match_4_id').val();
+      var next_match = $('.match_5_id').val();
       var next_match_player = 'player2';
     }
     else if(playerPosition == 6 || playerPosition == 7){
       var spotToFill = $roundThreeSpots.get(0);
+      var loserSpotToFill = $allPlayerSpots.get(8);
       var match_id = $('.match_4_id').val();
-      var next_match = $('.match_4_id').val();
-      var next_match_player = 'player2';
+      var next_match = $('.match_6_id').val();
+      var next_match_player = 'player1';
     }
     else if(playerPosition == 8 || playerPosition == 9){
       var spotToFill = $roundThreeSpots.get(1);
       var match_id = $('.match_5_id').val();
-      var next_match = $('.match_4_id').val();
+      var next_match = $('.match_6_id').val();
       var next_match_player = 'player2';
     }
-    // Handle the double match
+    // Winner bracket champ beats loser bracket champ
     else if(playerPosition == 10){
-      
-
+      var spotToFill = winner;
+      var match_id = $('.match_6_id');
+      //Just leave the unplayed match as everything nil
     }
-    // Handle the double match
+    // Loser bracket champ wins one, must play another match
     else if (playerPosition == 11){
+      var spotToFill = winner;
       // You must play another match, display indicator
       if (loserBracketChampWins == 0){
         loserBracketChampWins+=1;
+        $('.finals').show();
+        $('.indicator').text('Loser wins:' + loserBracketChampWins);
       }
       //loserBracketChamp has won tournament
-      else if (loserBracketChampWins == 2){
-
+      else if (loserBracketChampWins == 1){
+        loserBracketChampWins+=1;
+        var spotToFill = winner;
+        var match_id = $('.match_7_id');
       }  
     }
 
@@ -130,21 +139,38 @@ $('.player-de').on('click', function() {
   var playerName = $(this).text();
   var otherPlayerName = $(otherPlayer).text();
   if (playerName != 'bye'){
-    $(spotToFill).text(playerName);
+    if (tournamentSize == 4) {
+      if (playerPosition == 11 && (loserBracketChampWins == 0 || loserBracketChampWins == 1) ){
+        //Do nothing 
+      }
+      else {
+        $(spotToFill).text(playerName);
+      }
+    }
+    else if (tournamentSize == 8){
+
+    } 
+
   }
   //Stop bye from displaying as a winner
   else {
     $(spotToFill).text(otherPlayerName);
-      $(this).css('background-color', 'red');
-      $(otherPlayer).css('background-color', 'green');
+    $(this).css('background-color', 'red');
+    $(otherPlayer).css('background-color', 'green');
   }
+  if (loserSpotToFill) {
+    $(loserSpotToFill).text(otherPlayerName);
+  } 
 
   //Special case, illuminate champion spot upon choosing victor of finals 
-  if (playerPosition+1 == winnerPosition || playerPosition+2 == winnerPosition) {
+  if (playerPosition+2 == winnerPosition) {
+    $(winner).css('background-color', 'green');
+  }
+  else if (loserBracketChampWins == 2){
     $(winner).css('background-color', 'green');
   }
 
-  //AJAX to update the tournament's matches on the server
+  // AJAX to update the tournament's matches on the server
   // var tournament_id = $('.tournament_id').val();
   // var matchWinner = $(spotToFill).text(); 
   // var matchUpdateJSON = 
@@ -187,3 +213,4 @@ $('.player-de').on('click', function() {
   // }
 
 }
+});
